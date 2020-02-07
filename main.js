c0de4433eb644ad2545aac3877164a06939f43f7
obi3965 <5571142@RTS-365.dk>
2020-02-07 13:01:11 +0100

    data type of varaible

---
+++ main.js
@@ -0,0 +1,34 @@
+//  let fullName = 'obi'
+//  console.log(fullName);
+
+//  let fullAge = true;
+//  console.log(fullAge)
+
+// //undefined
+//  let job;
+//  console.log(job)
+//  job = 'Teacher';
+//  console.log(job)
+
+
+
+//VARIABLE MUTATION AND TYPE COERCION
+let firstName = 'obi';
+let age = 30;
+
+console.log(`${firstName}` + ' ' + `${age}`)
+
+
+let job, ismarried;
+job = 'teacher';
+ismarried = false;
+console.log(`${firstName}` + ' is a ' + `${age}` + ' years old ' + `${job}` + ' is he married ' + `${ismarried}`)
+
+
+//VARIABLE MUTATION
+age = 'twenty four';
+job = 'cleaner'
+alert(`${firstName}` + ' is a ' + `${age}` + ' years old ' + `${job}` + ' is he married ' + `${ismarried}`);
+
+let lastName = prompt('what is your last name')
+console.log(firstName + ' ' + lastName)
